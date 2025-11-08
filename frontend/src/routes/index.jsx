2092aa39e990d6c955b1ae30d45427fd11f854e9
vivekkr10 <vivekkr48027@gmail.com>
2025-11-08 18:41:36 +0530

    added complaint pages

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -14,18 +14,18 @@ import TL_UntouchedData from "../pages/salesTeamLead/UntouchedData";
 import TL_TeamMember from "../pages/salesTeamLead/TeamMember";
 import TL_ProspectForm from "../pages/salesTeamLead/ProspectForm";
 import TL_Prospect from "../pages/salesTeamLead/Prospect";
-import TL_Report from "../pages/salesTeamLead/Report"
+import TL_Report from "../pages/salesTeamLead/Report";
 
 // salesManager
-import SM_Dashboard from "../pages/salesManager/Dashboard"
+import SM_Dashboard from "../pages/salesManager/Dashboard";
 import SM_ManagerReport from "../pages/salesManager/managerReport";
 import SM_TotalSales from "../pages/salesManager/TotalSales";
 import SM_Salary from "../pages/salesManager/Salary";
 import SM_UntouchedData from "../pages/salesManager/UntouchedData";
 import SM_ProspectForm from "../pages/salesManager/ProspectForm";
 import SM_TeamMember from "../pages/salesManager/TeamMember";
-import SM_TeamReport from "../pages/salesManager/Report"
-import SM_TransferData from "../pages/salesManager/Transferdata"
+import SM_TeamReport from "../pages/salesManager/Report";
+import SM_TransferData from "../pages/salesManager/Transferdata";
 
 // managementTL
 import M_Dashboard from "../pages/managementTL/Dashboard";
@@ -36,12 +36,11 @@ import M_Report from "../pages/managementTL/Report";
 import M_Payout from "../pages/managementTL/Payout";
 import M_Review from "../pages/managementTL/Review";
 
-
 // managementEmployee
-import ME_Dashboard from "../pages/managementEmployee/Dashboard"
+import ME_Dashboard from "../pages/managementEmployee/Dashboard";
 import ME_CompleteProjects from "../pages/managementEmployee/CompleteProjects";
 import ME_PayOut from "../pages/managementEmployee/PayOut";
-import ME_Help from "../pages/managementEmployee/Help"
+import ME_Help from "../pages/managementEmployee/Help";
 
 // managerManagement
 import MM_Dashboard from "../pages/managerManagement/Dashboard";
@@ -55,15 +54,14 @@ import MM_WorkingProject from "../pages/managerManagement/WorkingProject";
 // feedbackManager
 import FM_Dashboard from "../pages/feedbackManager/Dashboard";
 import FM_Feedbacks from "../pages/feedbackManager/Feedbacks";
-// import FM_Salary from "../pages/feedbackManager/Salary";
+import FM_Complaint from "../pages/feedbackManager/complaints";
 
 // feedbackEmployee
 import FE_Dashboard from "../pages/feedBackEmployee/Dashboard";
-import FE_Salary from "../pages/feedBackEmployee/Salary"
+import FE_Salary from "../pages/feedBackEmployee/Salary";
 import FE_Feedbacks from "../pages/feedBackEmployee/Feedbacks";
 
 //feedbackManager
-import FM_Complaint from "../pages/feedbackManager/complaints";
 
 export const roleBasedRoutes = {
   salesExecutive: [
@@ -76,68 +74,63 @@ export const roleBasedRoutes = {
   ],
 
   salesTeamLead: [
-  { path: "/dashboard", element: <TL_Dashboard /> },
-  { path: "/transfer-data", element: <TL_TransferData /> },
-  { path: "/untouched-data", element: <TL_UntouchedData /> },
-  { path: "/prospect", element: <TL_Prospect /> },
-  { path: "/team-member", element: <TL_TeamMember /> },
-  { path: "/prospect-form", element: <TL_ProspectForm /> },
-  { path: "/report", element: <TL_Report /> },
+    { path: "/dashboard", element: <TL_Dashboard /> },
+    { path: "/transfer-data", element: <TL_TransferData /> },
+    { path: "/untouched-data", element: <TL_UntouchedData /> },
+    { path: "/prospect", element: <TL_Prospect /> },
+    { path: "/team-member", element: <TL_TeamMember /> },
+    { path: "/prospect-form", element: <TL_ProspectForm /> },
+    { path: "/report", element: <TL_Report /> },
   ],
 
   salesManager: [
-  { path: "/dashboard", element: <SM_Dashboard /> },
-  { path: "/manager-report", element: <SM_ManagerReport /> },
-  { path: "/total-sales", element: <SM_TotalSales /> },
-  { path: "/salary", element: <SM_Salary /> },
-  { path: "/untouched-data", element: <SM_UntouchedData /> },
-  { path: "/prospect-form", element: <SM_ProspectForm /> },
-  { path: "/team-member", element: <SM_TeamMember /> },
-  { path: "/report", element: <SM_TeamReport /> },
-  { path: "/transfer-data", element: <SM_TransferData /> },
+    { path: "/dashboard", element: <SM_Dashboard /> },
+    { path: "/manager-report", element: <SM_ManagerReport /> },
+    { path: "/total-sales", element: <SM_TotalSales /> },
+    { path: "/salary", element: <SM_Salary /> },
+    { path: "/untouched-data", element: <SM_UntouchedData /> },
+    { path: "/prospect-form", element: <SM_ProspectForm /> },
+    { path: "/team-member", element: <SM_TeamMember /> },
+    { path: "/report", element: <SM_TeamReport /> },
+    { path: "/transfer-data", element: <SM_TransferData /> },
   ],
-  
+
   managementTL: [
-  { path: "/dashboard", element: <M_Dashboard /> },
-  { path: "/projects", element: <M_MyProjects /> },
-  { path: "/transfer-projects", element: <M_TransferProjects /> },
-  { path: "/report", element: <M_Report /> },
-  { path: "/team-member", element: <M_TeamMember /> },
-  { path: "/payout", element: <M_Payout /> },
-  { path: "/review", element: <M_Review /> },
+    { path: "/dashboard", element: <M_Dashboard /> },
+    { path: "/projects", element: <M_MyProjects /> },
+    { path: "/transfer-projects", element: <M_TransferProjects /> },
+    { path: "/report", element: <M_Report /> },
+    { path: "/team-member", element: <M_TeamMember /> },
+    { path: "/payout", element: <M_Payout /> },
+    { path: "/review", element: <M_Review /> },
   ],
 
   managementEmployee: [
-  { path: "/dashboard", element: <ME_Dashboard /> },
-  { path: "/completed-projects", element: <ME_CompleteProjects /> },
-  { path: "/payout", element: <ME_PayOut /> },
-  { path: "/help", element: <ME_Help /> },
+    { path: "/dashboard", element: <ME_Dashboard /> },
+    { path: "/completed-projects", element: <ME_CompleteProjects /> },
+    { path: "/payout", element: <ME_PayOut /> },
+    { path: "/help", element: <ME_Help /> },
   ],
 
   managerManagement: [
-  { path: "/dashboard", element: <MM_Dashboard /> },
-  { path: "/manager-report", element: <MM_ManagerReport /> },
-  { path: "/recived-data", element: <MM_RecieveData /> },
-  { path: "/review", element: <MM_Review /> },
-  { path: "/total-projects", element: <MM_TotalProjects /> },
-  { path: "/transfer-data-to-feedback", element: <MM_TransferDataFDBK /> },
-  { path: "/working-projects", element: <MM_WorkingProject /> },
+    { path: "/dashboard", element: <MM_Dashboard /> },
+    { path: "/manager-report", element: <MM_ManagerReport /> },
+    { path: "/recived-data", element: <MM_RecieveData /> },
+    { path: "/review", element: <MM_Review /> },
+    { path: "/total-projects", element: <MM_TotalProjects /> },
+    { path: "/transfer-data-to-feedback", element: <MM_TransferDataFDBK /> },
+    { path: "/working-projects", element: <MM_WorkingProject /> },
   ],
 
   feedbackManager: [
-  { path: "/dashboard", element: <FM_Dashboard /> },
-  { path: "/feedbacks", element: <FM_Feedbacks /> },
-  // { path: "/salary", element: <FM_Salary /> },
+    { path: "/dashboard", element: <FM_Dashboard /> },
+    { path: "/feedbacks", element: <FM_Feedbacks /> },
+    { path: "/complaints", element: <FM_Complaint /> },
   ],
 
   feedbackEmployee: [
-  { path: "/dashboard", element: <FE_Dashboard /> },
+    { path: "/dashboard", element: <FE_Dashboard /> },
     { path: "/salary", element: <FE_Salary /> },
     { path: "/feedbacks", element: <FE_Feedbacks /> },
   ],
-
-  feedbackManager: [
-    {path: "/complaints", element: <FM_Complaint/>}
-  ],
-  
 };
